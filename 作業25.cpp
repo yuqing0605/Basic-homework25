69911ce5b5fd2f28be98b2ec0ade89f1c6e7331a
yuqing0605 <48577102+yuqing0605@users.noreply.github.com>
2019-05-31 11:04:49 +0800

    Add files via upload

---
+++ 作業25.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+	int i,j;
+	int *ptr[4];
+	int array[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+
+	for(i=0;i<4;i++){
+		ptr[i]=array[i];
+	}
+	
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+			printf("%3d", *(ptr[i]+j));
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+} 
